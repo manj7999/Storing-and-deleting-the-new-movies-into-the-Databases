8a9c76f03831c7a70e2115376e99bda44b16378b
Ankit Manjhi <ankitmanjhi286@gmail.com>
2024-02-17 16:58:50 +0530

    Loading the application

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformMovies = data.results.map(movieData => {
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films/")
+      const data = await response.json();
+
+        const transformMovies = data.results.map((movieData) => {
           return {
             id: movieData.episode_id,
             title: movieData.title,
@@ -21,7 +19,6 @@ function App() {
           };
         });
         setMovies(transformMovies);
-      });
   }
   return (
     <React.Fragment>
