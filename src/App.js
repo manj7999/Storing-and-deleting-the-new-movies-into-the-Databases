ac56bff498922822e36624bb8bbc6e850933da00
Ankit Manjhi <ankitmanjhi286@gmail.com>
2024-02-17 23:43:36 +0530

    Storing and deleting the new movies into the Databases

--- src/App.js
+++ src/App.js
@@ -1,63 +1,76 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [newMovie, setNewMovie] = useState({
-    title: "",
-    openingText: "",
-    releaseDate: "",
-  });
 
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
-
+      const response = await fetch(
+        "https://react-http-aa797-default-rtdb.firebaseio.com/movies.json"
+      );
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
 
       const data = await response.json();
 
-      const transformMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseData: movieData.release_date,
-        };
-      });
-      setMovies(transformMovies);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingtext: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      // const transformedMovies = data.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //   };
+      // });
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
-    } finally {
-      setIsLoading(false);
     }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://react-http-aa797-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
   }
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setNewMovie((prevMovie) => ({
-      ...prevMovie,
-      [name]: value,
-    }));
-  };
-
-  const handleAddMovie = () => {
-    console.log(newMovie);
-    // Here you can add the new movie to your list or perform any other action
-  };
 
   let content = <p>Found no movies.</p>;
 
-  if (movies.length) {
-    content = <moviesList movies={movies} />;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
   }
 
   if (error) {
@@ -71,37 +84,9 @@ function App() {
   return (
     <React.Fragment>
       <section>
-        <form>
-          <div>
-            <label>Title:</label>
-            <input
-              type="text"
-              name="title"
-              value={newMovie.title}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <label>Opening Text:</label>
-            <textarea
-              name="openingText"
-              value={newMovie.openingText}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <label>Release Date:</label>
-            <input
-              type="text"
-              name="releaseDate"
-              value={newMovie.releaseDate}
-              onChange={handleInputChange}
-            />
-          </div>
-          <button type="button" onClick={handleAddMovie}>
-            Add Movie
-          </button>
-        </form>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>{content}</section>
