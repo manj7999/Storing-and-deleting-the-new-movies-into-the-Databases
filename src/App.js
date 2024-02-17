208582626bed25927a11a7946932a29e7353c9b4
Ankit Manjhi <ankitmanjhi286@gmail.com>
2024-02-17 21:07:16 +0530

    Making apps error proof

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
-    const response = await fetch("https://swapi.dev/api/films/")
-      const data = await response.json();
+    setIsLoading(true);
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
 
-        const transformMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseData: movieData.release_date,
-          };
-        });
-        setMovies(transformMovies);
+    const transformMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseData: movieData.release_date,
+      };
+    });
+    setMovies(transformMovies);
+    setIsLoading(false);
   }
   return (
     <React.Fragment>
@@ -26,7 +29,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.lnegth > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 &&  <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
